da9842777d3c80da17fb57cf2641f5058fe33457
SleekMind <163165737+SleekMind@users.noreply.github.com>
2024-06-14 23:29:24 +0530

    finally solved :)

--- C_Number_of_Pairs.cpp
+++ C_Number_of_Pairs.cpp
@@ -1,4 +1,4 @@
-// Vinayak Sonar ॐ 
+// Vinayak Sonar ॐ
 #include <bits/stdc++.h>
 using namespace std;
 
@@ -9,13 +9,31 @@ using namespace std;
 
 void vinayak()
 {
-    
+    int n, l, r;
+    cin >> n >> l >> r;
+
+    vector<int> v(n);
+    for (int i = 0; i < n; i++)
+        cin >> v[i];
+
+    sort(v.begin(), v.end());
+    int ans = 0;
+
+    for (int i = 0; i < n; i++)
+    {
+
+        int k = lower_bound(v.begin() + i + 1, v.end(), l - v[i]) - v.begin();
+        int q = upper_bound(v.begin() + i + 1, v.end(), r - v[i]) - v.begin();
+
+        ans = ans + q - k;
+    }
+    cout << ans << endl;
 }
 
 signed main()
 {
     ios_base::sync_with_stdio(false); // don't use while debugging
-    cin.tie(NULL); // don't use in interactive problems
+    cin.tie(NULL);                    // don't use in interactive problems
     cout.tie(NULL);
     int t = 1;
     cin >> t;
